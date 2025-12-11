d0215b383a284c83cdd0947d46c667f857bd20ce
Luke Morton <lukem32ca@gmail.com>
2025-12-11 14:47:06 -0700

    initial creation commit

---
+++ src/main/java/com/lukemorton/CommentsApplicationTests.java
@@ -0,0 +1,14 @@
+// package com.lukemorton;
+
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
+
+// @SpringBootTest
+// class CommentsApplicationTests {
+
+// 	@Test
+// 	void contextLoads() {
+// 	}
+
+// }
+
